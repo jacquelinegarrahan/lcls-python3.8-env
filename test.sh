f5c899243e9890358d81220cda282653e5e8223b
Jacqueline Garrahan <jacquelinegarrahan@gmail.com>
2021-11-24 09:26:05 -0800

    Check in workflow with new testing

---
+++ test.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+python - <<EOF
+import pysocks
+import parso
+import matplotlib
+import pillow
+import tornado
+import qtpy
+import statsmodels
+import prometheus_client
+import tensorflow
+import yaml
+import keras
+import pydm
+import pyqt
+import pandas
+import bokeh
+import pyqtprocess
+import pyqtgraph
+import epics
+import numpy
+import theano
+import pasta
+import p4p
+import sklearn
+import pytao
+import treelib
+import pytest
+EOF
