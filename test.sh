0163b6ff444234da54b4e7b8bb90c3c7b107629c
Jacqueline Garrahan <jacquelinegarrahan@gmail.com>
2021-11-24 10:19:34 -0800

    Remove pillow

--- test.sh
+++ test.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
 python - <<EOF
 import matplotlib
-import pillow
 import tornado
 import qtpy
 import statsmodels
 import prometheus_client
 import tensorflow
-import yaml
 import keras
 import pydm
 import pyqt
 import pandas
 import bokeh
-import pyqtprocess
-import pyqtgraph
 import epics
 import numpy
 import theano
